5117b12e6f2f48f3c98f170a1e06af27ebf9c1c8
“deannabosschert” <deanna.bosschert@gmail.com>
2021-01-27 19:40:41 +0100

    feat: update _site

--- _site/js/modules/static/map-data-po copy.js
+++
@@ -1,8 +0,0 @@
-export function mapDataPO(data) {
-  return data.map(item => ({
-    AreaId: item.AreaId,
-    AreaManagerId: item.AreaManagerId,
-    OpenAllYear: item.OpenAllYear,
-    ExitPossibleAllDay: item.ExitPossibleAllDay
-  }));
-}
